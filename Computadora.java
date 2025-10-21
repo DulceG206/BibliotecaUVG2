45b8b69f6fb4f54671a5ddfb3aee1842627f17d3
Carlos De Leon <123278258+carlos0654@users.noreply.github.com>
2025-10-21 07:11:51 -0600

    segundo

---
+++ Computadora.java
@@ -0,0 +1,20 @@
+
+
+public class Computadora {
+    
+
+private int noSerie;
+public boolean enUso; 
+public String marca;
+
+public Computadora (int noSerie,  String marca ) {
+}
+public int getNoSerie () {
+    return noSerie;
+}
+public boolean getEnUso () {
+    return false;
+}
+public void apagar () {
+}
+}
